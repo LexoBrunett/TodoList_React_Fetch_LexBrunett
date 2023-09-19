34758b79f110aca598e2a3bcddc502e273471b91
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-19 00:32:46 +0000

    todo list

---
+++ src/js/API.js
@@ -0,0 +1,14 @@
+export const addTask = (tasksAPI) => {
+  fetch("https://assets.breatheco.de/apis/fake/todos/user/Toni_CM", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(tasksAPI) 
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+ };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import List from "./List";
+import { createUser, addTask, getAPI, } from "../API";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [remainingTasks, setRemainingTasks] = useState(0);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim() === "") {
+      alert("No se agrega tarea vacía");
+      return;
+    }
+
+    const newTask = {
+      id: uuidv4(),
+      label: input,  
+      done: false,     
+      
+    };
+    
+    setTasks([newTask, ...tasks]);
+    setInput("");
+    if(tasks.length >= 0) {setRemainingTasks(remainingTasks+1)}
+  };
+
+  // --------------------------------
+
+  // FUNCION PARA ELIMINAR TAREA
+
+  const onDeleteTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+    if(remainingTasks > 0) {
+    setRemainingTasks(remainingTasks-1)}
+  };
+  
+// --------------------------------
+
+// LLAMADA A LA API
+const getAPI = () => {
+  return fetch("https://assets.breatheco.de/apis/fake/todos/user/LexoBrunett", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        createUser();
+      }
+    })
+    .then((data) => {
+      setTasks(data);
+      setRemainingTasks(data.length);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+
+// --------------------------------
+
+// UTILIZACIÓN USEEFFECT
+
+useEffect(() => {
+  getAPI()
+}, []);
+
+
+useEffect(() => {
+  addTask(tasks)
+},[tasks])
+
+
+
+// --------------------------------
+
+  return (
+    <div>
+      <h1><strong>To-Do List with API fetch LexoBrunett</strong></h1>
+      <form className="form-mainContainer" onSubmit={handleSubmit}>
+        <input
+          className="task-Input"
+          type="text"
+          placeholder="Escribe una tarea..."
+          name="texto"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <List tasks={tasks} onDeleteTask={onDeleteTask} />
+        <p className="recountTask"><Strong>Pending Tasks</Strong>{remainingTasks}</p>
+      </form>
+      <button className="createDeleteButton" onClick={() => { setTasks([]), setRemainingTasks(0) }}>
+        Limpiar Tareas
+      </button>
+    </div>
+  );
 };
 
 export default Home;
